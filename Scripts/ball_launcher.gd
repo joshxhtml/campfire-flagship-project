28b2936d46dc41b29e6da626c752f6b71dfad729
Josh Worthington <joshuatw423@gmail.com>
2026-01-04 23:59:43 -0500

    New Physics
    uploading to github so i save this in the unfortunate case my laptop blows up

--- Scripts/ball_launcher.gd
+++ Scripts/ball_launcher.gd
@@ -9,21 +9,20 @@ extends Node3D
 @export var min_launch_force := 8.0
 @export var max_launch_force := 28.0
 
+@export var max_angle_random := .04 #something something radians are 2.3 degrees, this is imporant and will help us later in tuning it to be right, remember this josh, thats why theres a big ass comment here so you remember to come back 👌
+@export var power_random := 0.03 #3 percent
+@export var spin_strenght := 8
+@export var position_changer := 0.01
+
 var position_input := 0.0
 var tilt_input := 0.0
 var power := 0.0 
 var tilt_mode := false
 var charging := false
 
-
-
 @export var ball_scene: PackedScene
-
 @export var aim_line_lenght := 6.0
-
-
 @onready var aim_line := $AimLine
-
 
 func update_aim_line():
 	var mesh := ImmediateMesh.new()
@@ -44,7 +43,6 @@ func update_aim_line():
 	
 	mesh.surface_add_vertex(start)
 	mesh.surface_add_vertex(end)
-	
 	mesh.surface_end()
 	
 	$AimLine.mesh = mesh
@@ -63,10 +61,13 @@ func _process(delta):
 		
 	position.x = position_input
 	rotation.z = deg_to_rad(-tilt_input)
+	
 	power_bar.value = power * 100.0
+	
 	if Input.is_action_pressed("shoot"):
 		charging = true
 		power = min(power + delta * 0.8, 1.0)
+		
 	elif charging:
 		shoot_ball()
 		charging = false
@@ -84,12 +85,34 @@ func shoot_ball():
 	
 	var ball = ball_scene.instantiate()
 	get_parent().add_child(ball)
-	ball.global_position = global_position
 	
-	var direction = -global_transform.basis.z.normalized()
-	var launch_force = lerp(min_launch_force, max_launch_force, power)
+	var spawn_pos = global_position
+	spawn_pos.x += randf_range(-position_changer, position_changer)
+	spawn_pos.z += randf_range(-position_changer, position_changer)
+	ball.global_position = spawn_pos
 	
-	ball.apply_impulse(direction * launch_force)
-	print("Launch force:", launch_force)
+	var base_direction = -global_transform.basis.z.normalized()
 	
-	power = 0.0
+	#fuck the player, thats right, all your throws are now random, no more stragety, its all out the window
+	var angle_offset = randf_range(-max_angle_random, max_angle_random)
+	var lauch_dir = base_direction.rotated(Vector3.UP, angle_offset)
+	
+	var base_force = lerp(min_launch_force, max_launch_force, power)
+	var final_force = base_force * randf_range( 1.0 - power_random, 1.0 + power_random)
+	
+	ball.angular_velocity = Vector3(
+		randf_range(-spin_strenght, spin_strenght),
+		randf_range(-1.0, 1.0),
+		randf_range(-spin_strenght, spin_strenght)
+	)
+	
+	ball.apply_impulse(lauch_dir * final_force)
+	print("Launch force:", final_force)
+	
+	#old logic, saving just in case
+	#ball.global_position = global_position
+	#var direction = -global_transform.basis.z.normalized()
+	#var launch_force = lerp(min_launch_force, max_launch_force, power)
+	#ball.apply_impulse(direction * launch_force)
+	#print("Launch force:", launch_force)
+	#power = 0.0
